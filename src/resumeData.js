add395eb80a6e26902cc0e61b12bb2c34839ea64
ty_shiva93 <shiva.k@testyantra.in>
2022-10-28 15:09:02 +0530

    First Commit

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Dhanush Reddy S",
+  role: "Full Stack Developer (MERN)",
+  linkedinId: "https://www.linkedin.com/in/dhanush-reddy-32b915191/",
+  skypeid: "https://join.skype.com/invite/he8fIwgczq7d",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/dhanush-reddy-32b915191/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Dhanush-reddy-1831",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "https://join.skype.com/invite/he8fIwgczq7d",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am currently working with Technoelevate which is part of TestYanta Company. I am trained with Full Stack Web Developer (MERN). I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "India",
+  education: [
+    {
+      UniversityName: "SJR Degree College",
+      specialization: "BCA In Computer-Science",
+      MonthOfPassing: "Sep",
+      YearOfPassing: "2021",
+      Achievements: "participated in Intercollege competition.",
+    },
+    {
+      UniversityName: "SJR PU College",
+      specialization: "PCMC",
+      MonthOfPassing: "JUN",
+      YearOfPassing: "2018",
+      Achievements:
+        "participated in Intercollege competition and won some prizes and State-level Carrom.",
+    },
+    {
+      UniversityName: "SR English School",
+      MonthOfPassing: "May",
+      YearOfPassing: "2016",
+      Achievements: "participated in State-level competition",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Test Yantra",
+      specialization: "MERN",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2022",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "React_js",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Shop_IT",
+      description: "Web application",
+      imgurl: "images/portfolio/project1.JPEG",
+    },
+    {
+      name: "Shop_IT",
+      description: "Web application",
+      imgurl: "images/portfolio/project2.JPEG",
+    },
+    {
+      name: "Shop_IT",
+      description: "Web application",
+      imgurl: "images/portfolio/project3.JPEG",
+    },
+  ],
+  testimonials: [
+    {
+      description:
+        "“I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you. It's really great how easy your websites are to update and manage.",
+      name: "Tejabhinandan Nayaka",
+    },
+  ],
+};
+
+export default resumeData;
